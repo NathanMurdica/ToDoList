7e48213ba6734e529de7670facf3812f8c830ff9
The Tater Lord <161768238+ReubenHolmes@users.noreply.github.com>
2025-10-23 12:37:14 +1100

    create update_db()

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from enum import Enum
-# from fastapi.middleware.cors import CORSMiddleware
+import json
 
 app = FastAPI()
 
@@ -26,27 +26,29 @@ class Todo(BaseModel):
     updated_at = None
 
 
-todo_list = []
+todo_list: Todo = []
 
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-
+def update_db():
+    # Convert todo_list into a json stream and overwrite the file
+    json_str = json.dumps(todo_list, indent=4)
+    with open("../database/products.json", "w") as db:
+        db.write(json_str)
 
 
 @app.post("/todo_list/")
 def create_todo(todo: str):
     todo_list.append(todo)
+    update_db()
     return todo
 
 
 @app.get("/todo_list", response_model=list[Todo])
 def list_todos(limit: int = 63):
+    if len(todo_list) == 0:
+        with open("../database/products.json", "r") as db:
+            todo_list = json.load(db)
+
     return todo_list[0:limit]
 
 
